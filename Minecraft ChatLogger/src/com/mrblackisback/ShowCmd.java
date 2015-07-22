7a43fd68de655a59d8d367c344b5e384c2320b9c
BuildTools <jamesbehan198@gmail.com>
2015-07-22 14:31:36 +0100

    Added pages.

--- Minecraft ChatLogger/src/com/mrblackisback/ShowCmd.java
+++ Minecraft ChatLogger/src/com/mrblackisback/ShowCmd.java
@@ -3,6 +3,9 @@ package com.mrblackisback;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Map.Entry;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -22,13 +25,14 @@ public class ShowCmd implements CommandExecutor {
 
 		if (cmd.getName().equalsIgnoreCase("show")) {
 			if (sender.hasPermission("chatlogger.show")) {
-				if (args.length == 0 || args.length > 1) {
-					sender.sendMessage(ChatColor.RED + "/show [player]");
+				if (args.length == 0 || args.length > 2) {
+					sender.sendMessage(ChatColor.RED + "/show [player] <number>");
 					return true;
 				}
 
 				Player t = Bukkit.getPlayer(args[0]);
 				FileHandler hand;
+				SortedMap<Integer, String> page = new TreeMap<Integer, String>();
 
 				if (t != null) {
 					hand = new FileHandler(t.getName(), main.getDataFolder());
@@ -37,10 +41,38 @@ public class ShowCmd implements CommandExecutor {
 						BufferedReader read = new BufferedReader(new FileReader(hand.getFile()));
 
 						String line = "";
+						int l = 1;
 
 						while ((line = read.readLine()) != null) {
-							sender.sendMessage(ChatColor.translateAlternateColorCodes('&', line));
+
+							page.put(l, line);
+							l++;
 						}
+
+						int num = 1;
+
+						try {
+							if (args.length == 2) {
+								num = Integer.parseInt(args[1]);
+							} else {
+								num = 1;
+							}
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+
+						if (l > 1) {
+							paginate(sender, page, num, 5);
+						} else {
+							paginate(sender, page, 1, 5);
+						}
+						// while ((line = read.readLine()) != null) {
+						// page.put(l, line);
+						// paginate(sender, page, 1, 5);
+						// sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+						// line));
+						// l++;
+						// }
 
 						read.close();
 					} catch (IOException e) {
@@ -57,6 +89,21 @@ public class ShowCmd implements CommandExecutor {
 			return true;
 		}
 		return false;
+	}
+
+	public void paginate(CommandSender sender, SortedMap<Integer, String> map, int page, int pageLength) {
+		int i = 0, k = 0;
+		page--;
+		for (final Entry<Integer, String> e : map.entrySet()) {
+			k++;
+			if ((((page * pageLength) + i + 1) == k) && (k != ((page * pageLength) + pageLength + 1))) {
+				i++;
+				sender.sendMessage(e.getValue());
+			}
+		}
+		sender.sendMessage(ChatColor.GOLD + "" + (page + 1) + " / "
+				+ (((map.size() % pageLength) == 0) ? map.size() / pageLength : (map.size() / pageLength) + 1)
+				+ ChatColor.GREEN + " pages");
 	}
 
 }
